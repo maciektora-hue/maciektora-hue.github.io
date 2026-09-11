6a6de232efad1326214ccb90899c2be1afd1a670
maciektora-hue <maciek.tora@gmail.com>
2026-09-11 16:16:20 +0200

    Remove one-time Liked Songs import endpoint registration

--- piosenki/playlist_api.py
+++ piosenki/playlist_api.py
@@ -95,7 +95,3 @@ def api_playlisty():
     finally:
         if conn is not None:
             conn.close()
-
-
-# Tymczasowy, uwierzytelniony endpoint jednorazowego importu Liked Songs.
-import liked_songs_import_once  # noqa: E402,F401
