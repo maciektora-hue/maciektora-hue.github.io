785438de9ab3c0b3f3cec7cc8e5036be0b510f36
maciektora-hue <maciek.tora@gmail.com>
2026-09-12 06:57:41 +0200

    Trigger Supabase single INSERT

--- .github/sol-data/supabase-single-insert.sql
+++ .github/sol-data/supabase-single-insert.sql
@@ -3,3 +3,4 @@ INSERT INTO tag_snapshots VALUES(
 '2026-09-06',
 'przemiana:bardzo; cialo:bardzo; seks:bardzo; performowanie-roli:bardzo; kontrola-wizerunku:bardzo; nigdy-dosc:bardzo; wstret:bardzo; do-innych:bardzo; ku-swiatu:bardzo; sprawczosc:bardzo; taniec:troche; bunt:troche; innosc:troche'
 );
+
